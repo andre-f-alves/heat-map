bccbe3560a3ac7cf668c54d0b1687d306a105fe1
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-03-01 16:03:24 -0300

    adding y axis

--- script.js
+++ script.js
@@ -4,25 +4,44 @@ const endpoint = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenc
 
 const data = await d3.json(endpoint)
 
-const [ minYear, maxYear ] = d3.extent(data.monthlyVariance, item => item.year)
+const [minYear, maxYear] = d3.extent(data.monthlyVariance, item => item.year)
 
 d3.select('#description')
   .text(`${minYear} - ${maxYear}: base temperature ${data.baseTemperature}°C`)
 
 const width = 1200
 const height = 700
-const padding = 30
+const padding = 60
 
 const svg = d3.select('.svg-container')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
 
-const xScale = d3.scaleTime(
-  [new Date(minYear, 0), new Date(maxYear, 0)],
+const xScale = d3.scaleLinear(
+  [minYear, maxYear],
   [padding, width - padding]
 )
 
+const yScale = d3.scaleBand(
+  Array(12).fill(0).map((_, index) => index),
+  [height - padding, padding]
+)
+
+const xAxis = d3.axisBottom(xScale)
+  .tickFormat(d3.format(''))
+  .ticks(20)
+
+const yAxis = d3.axisLeft(yScale)
+  .tickFormat(number => {
+    const month = d3.timeMonth().setMonth(number)
+    return d3.timeFormat('%B')(month)
+  })
+
 svg.append('g')
   .attr('transform', `translate(0, ${height - padding})`)
-  .call(d3.axisBottom(xScale).ticks(20))
+  .call(xAxis)
+
+svg.append('g')
+  .attr('transform', `translate(${padding}, 0)`)
+  .call(yAxis)
